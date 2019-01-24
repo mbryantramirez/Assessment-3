b86ee2f548d6d05dfac26742a3065a948127ea88
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-01-23 22:28:50 -0500

    finishing what in time constraint

--- app/src/main/java/org/pursuit/unit_03_assessment/DisplayActivity.java
+++ app/src/main/java/org/pursuit/unit_03_assessment/DisplayActivity.java
@@ -3,14 +3,31 @@ package org.pursuit.unit_03_assessment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
 
+import org.pursuit.unit_03_assessment.Model.ArrayList_Planets;
+import org.pursuit.unit_03_assessment.Network.Service_Planet;
 import org.pursuit.unit_03_assessment.R;
+import org.pursuit.unit_03_assessment.Singleton.Singleton_Retrofit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class DisplayActivity extends AppCompatActivity {
     private TextView nameTextView;
     private TextView numberTextView;
     private TextView imageTextView;
+    RecyclerView recyclerView;
+    MyAdapter adapter;
+    private List<Planet> planets = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +42,7 @@ public class DisplayActivity extends AppCompatActivity {
 
 
         nameTextView.setText(getIntent().getStringExtra("name"));
-                numberTextView.setText(getIntent().getStringExtra("number"));
+        numberTextView.setText(getIntent().getStringExtra("number"));
         imageTextView.setText(getIntent().getStringExtra("image"));
 
 
@@ -35,4 +52,33 @@ public class DisplayActivity extends AppCompatActivity {
 //        * TODO 5. Display each value in views - Strings for TextViews, and Picasso for the ImageView
 //        */
 
+//retrofit instance
+        Retrofit retrofit = Singleton_Retrofit.retrofitInstance();
+//.enqueue makes the retrofit run on the back thread
+        retrofit.create(Service_Planet.class)
+                .getPlanet()
+                .enqueue(new Callback<ArrayList_Planets>() {
+                    @Override
+                    public void onResponse(Call<ArrayList_Planets> call, Response<ArrayList_Planets> response) {
+                        Log.d("TAG", "onResponse: " + response.body()
+                                .getPlanets()
+                                .get(0)// first object of the Json arraylist
+                                .getName()); // the name element of the Json object
+                        for (int i = 0; i < response.body().getPlanets().size(); i++) {
+                            planets.add(response.body().getPlanets().get(i));
+                        }
+                        Log.d("TAG", "size: " + planets.size());
+
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    }
+
+
+                    @Override
+                    public void onFailure(Call<ArrayList_Planets> call, Throwable t) {
+                        Log.d("TAG", "onResponse: " + t.toString());
+                    }
+                });
+
     }
+}
